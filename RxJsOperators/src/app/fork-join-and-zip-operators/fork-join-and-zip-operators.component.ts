59cd6db2b0eb45dd0b143fba05327458d8af2261
unknown <oficinadosdrones@gmail.com>
2022-02-22 10:22:33 +0000

    forkJoin

--- RxJsOperators/src/app/fork-join-and-zip-operators/fork-join-and-zip-operators.component.ts
+++ RxJsOperators/src/app/fork-join-and-zip-operators/fork-join-and-zip-operators.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { delay, forkJoin, Subscription } from 'rxjs';
+import { ForkZipService } from './fork-zip.service';
 
 @Component({
   selector: 'app-fork-join-and-zip-operators',
   templateUrl: './fork-join-and-zip-operators.component.html',
-  styleUrls: ['./fork-join-and-zip-operators.component.css']
 })
 export class ForkJoinAndZipOperatorsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  
+  constructor(private forkJoinService: ForkZipService) {
+    
   }
 
+  ngOnInit(): void {
+    /**
+     * Como paramentro do ForkJoin, temos que passar a FUNÇÂO e não a subscrição. Lembrando que 
+     * o RETORNO é sempre um ARRAY,
+     * Colocaremos um DELAY para mostrar que o FORKJOIN, só retorna depois que TODOS os 
+     * OBSERVABLES estiverem retornado.
+     */
+    forkJoin([
+      this.forkJoinService.getUmBacon().pipe(delay(3000)),
+      this.forkJoinService.getDoisBacon(),
+    ]).subscribe(([data01, data02]) => {console.log(data01, data02) });
+  }
 }
--- RxJsOperators/src/app/fork-join-and-zip-operators/fork-zip.service.ts
+++ RxJsOperators/src/app/fork-join-and-zip-operators/fork-zip.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ForkZipService {
-
-  constructor() { }
+  constructor(private httpService: HttpClient) {}
+  getUmBacon() {
+    return this.httpService.get(
+      `https://baconipsum.com/api/?type=meat-and-filler`
+    );
+  }
+  getDoisBacon() {
+    return this.httpService.get(
+      `https://baconipsum.com/api/?type=all-meat&paras=2&start-with-lorem=1`
+    );
+  }
 }
